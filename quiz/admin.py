cd80e915912b9ca931d6e6188f5faf03bdabe68f
brs14shn <brsshn292929@gmail.com>
2022-09-26 20:39:50 +0300

    admin

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import (
+Category,
+Quiz,
+Question,
+Option
+)
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Quiz)
+admin.site.register(Question)
+admin.site.register(Option)
+
--- quiz/models.py
+++ quiz/models.py
@@ -1,19 +1,57 @@
+
 from django.db import models
 
-# Create your models here.
-class Category(models.Model):
-    name = models.CharField(max_length=50,verbose_name="Category Name")
-    
-    def __str__(self):
-        return self.name 
+class UpdateCreateDate(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now = True)
+
     class Meta:
-        verbose_name_plural="Categories"
+        abstract = True
 
 
-class Quiz(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    title = models.CharField(max_length=20,verbose_name="Quiz title")
-    date_created = models.DateTimeField(auto_now_add=True, null=True)
+class Category(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Category Name')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
+
+class Quiz(UpdateCreateDate):
+    title = models.CharField(max_length=50, verbose_name = 'Quiz Title')
+    category = models.ForeignKey(Category, on_delete = models.CASCADE)
+    #? delete questions when category changes (models.CASCADE) 👆
     
+
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name_plural = 'Quizzes'
+        
+
+class Question(UpdateCreateDate):
+    SCALE = (
+        ('B', 'Beginner'),
+        ('I', 'Intermadiate'),
+        ('A', 'Advanced')
+    )
+    title = models.TextField()
+    quiz = models.ForeignKey(Quiz, on_delete = models.CASCADE)
+    difficulty = models.CharField(max_length = 1, choices = SCALE)
+    
+
+    def __str__(self):
+        return self.title
+
+class Option(UpdateCreateDate):
+    option_text = models.CharField(max_length = 200)
+    question = models.ForeignKey(Question, on_delete = models.CASCADE)
+    is_right = models.BooleanField(default=False)
+    
+
+    def __str__(self):
+        return self.option_text
